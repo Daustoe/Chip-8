e39e47e63185eebe4e5760de073f5de660c14022
Clayton Powell <daustoe@gmail.com>
2014-06-24 17:02:29 -0700

    tweaking the disassembler, trying to find out what I want to do with it.

--- chip-8_disassembler.py
+++ chip-8_disassembler.py
@@ -5,18 +5,37 @@ class Disassembler(object):
     def __init__(self, rom_path):
         self.memory = [0] * 4096
         self.load_rom(rom_path)
-        self.disassemble()
+        self.op_map = {0x0000: 'not handled',
+                       0x1000: 'JUMP',
+                       0x2000: 'CALL',
+                       0x3000: 'SKIP.EQ',
+                       0x4000: 'SKIP.NE',
+                       0x5000: 'SKIP.EQ',
+                       0x6000: 'MVI',
+                       0x7000: 'ADI',
+                       0x8000: 'not handled',
+                       0x9000: 'SKIP.NE',
+                       0xA000: 'MVI',
+                       0xB000: 'JUMP',
+                       0xC000: 'RNDMASK',
+                       0xD000: 'DRAW',
+                       0xE000: 'SKIP.KEY',
+                       0xF000: 'not handled',
+                       }
 
     def load_rom(self, rom_path):
         rom = open(rom_path, "rb").read()
         for index in range(0, len(rom)):
-            self.memory[index] = ord(rom[index])
+            self.memory[index + 0x200] = ord(rom[index])
 
     def disassemble(self):
-        for index in range(0, len(self.memory), 2):
+        for index in range(0x200, len(self.memory), 2):
             opcode = (self.memory[index] << 8) | self.memory[index+1]
-            print hex(opcode)
-            print hex(opcode & 0xf000)
+            print '{} {} {}'.format(hex(index), hex(opcode), self.disect_op(opcode))
+
+    def disect_op(self, opcode):
+        return self.op_map[opcode & 0xf000]
 
 if __name__ == '__main__':
-    dsm = Disassembler('games/INVADERS')
+    dsm = Disassembler('games/PONG')
+    dsm.disassemble()
