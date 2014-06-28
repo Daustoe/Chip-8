7191240b36890fb6477522a7a3cec788e5c3930b
Clayton Powell <daustoe@gmail.com>
2014-06-28 00:12:24 -0700

    Started adding docstrings
    Adding docstrings and renaming to follow PEP-8 standards

--- disassembler.py
+++ disassembler.py
@@ -2,17 +2,21 @@ __author__ = 'Claymore'
 
 
 class Disassembler(object):
+    """
+    Chip8 opcode disassembler.
+    """
+
     def __init__(self):
         self.memory = [0] * 4096
         self.opcode = 0x0
-        self.op_map = {0x0000: self._0ZZZ,
-                       0x00e0: self._00E0,
-                       0x00ee: self._00EE,
-                       0x1000: self._1NNN,
-                       0x2000: self._2NNN,
-                       0x3000: self._3XNN,
-                       0x4000: self._4XNN,
-                       0x5000: self._5XY0,
+        self.op_map = {0x0000: self._0zzz,
+                       0x00e0: self._00e0,
+                       0x00ee: self._00ee,
+                       0x1000: self._1nnn,
+                       0x2000: self._2nnn,
+                       0x3000: self._3xnn,
+                       0x4000: self._4xnn,
+                       0x5000: self._5xy0,
                        0x6000: self._6XNN,
                        0x7000: self._7XNN,
                        0x8000: self._8ZZZ,
@@ -46,151 +50,167 @@ class Disassembler(object):
                        }
 
     def load_rom(self, rom_path):
+        """
+        Loads Chip8 rom from given path into local memory.
+        :param rom_path:
+        """
         rom = open(rom_path, "rb").read()
         for index in range(0, len(rom)):
             self.memory[index + 0x200] = ord(rom[index])
 
     def disassemble(self, pc, opcode):
+        """
+        Disassembles a single opcode, requires program counter for printing reference.
+        :param pc:
+        :param opcode:
+        :return:
+            Returns string with details about that particular opcode
+        """
         self.opcode = opcode
         if opcode == 0:
             return '%s %s\t\tNOP' % (hex(pc), str(hex(opcode))[2:])
         else:
             try:
-                return '%s %s\t%s' % (hex(pc), str(hex(opcode))[2:], self.op_map[opcode & 0xf000]())
+                function = self.op_map[opcode & 0xf000]
+                return '%s %s\t%s' % (hex(pc), str(hex(opcode))[2:], function())
             except KeyError:
                 return '%s %s\t%s' % (hex(pc), str(hex(opcode))[2:], '\tNot Handled')
 
     def disassemble_all(self):
+        """
+        Disassembles the entire rom of a given Chip8 rom.
+        """
         for index in range(0x200, len(self.memory), 2):
             self.opcode = (self.memory[index] << 8) | self.memory[index+1]
             print self.disassemble(index, self.opcode)
 
-    def extract(self, mask):
+    def _extract(self, mask):
         return str(hex(self.opcode & mask))
 
-    def extract_vx_vy(self):
-        return self.extract(0xf00)[2], self.extract(0xf0)[2]
+    def _extract_vx_vy(self):
+        return self._extract(0xf00)[2], self._extract(0xf0)[2]
 
-    def extract_vx_nn(self):
-        return self.extract(0xf00)[2], self.extract(0xff)[2:]
+    def _extract_vx_nn(self):
+        return self._extract(0xf00)[2], self._extract(0xff)[2:]
 
-    def _0ZZZ(self):
-        return self.op_map[self.opcode & 0xf0ff]()
+    def _0zzz(self):
+        function = self.op_map[self.opcode & 0xf0ff]
+        return function()
 
-    def _00E0(self):
+    @staticmethod
+    def _00e0():
         return 'CLS'
 
-    def _00EE(self):
+    @staticmethod
+    def _00ee():
         return 'RTS'
 
-    def _1NNN(self):
-        return 'JUMP\t\t$%s' % self.extract(0xfff)[2:]
+    def _1nnn(self):
+        return 'JUMP\t\t$%s' % self._extract(0xfff)[2:]
 
-    def _2NNN(self):
-        return 'CALL\t\t$%s' % self.extract(0x0fff)[2:]
+    def _2nnn(self):
+        return 'CALL\t\t$%s' % self._extract(0x0fff)[2:]
 
-    def _3XNN(self):
-        return 'SKIP.EQ\t\tV%s, \t$%s' % self.extract_vx_nn()
+    def _3xnn(self):
+        return 'SKIP.EQ\t\tV%s, \t$%s' % self._extract_vx_nn()
 
-    def _4XNN(self):
-        return 'SKIP.NE\t\tV%s, \t$%s' % self.extract_vx_nn()
+    def _4xnn(self):
+        return 'SKIP.NE\t\tV%s, \t$%s' % self._extract_vx_nn()
 
-    def _5XY0(self):
-        return 'SKIP.EQ\t\tV%s, \tV%s' % self.extract_vx_vy()
+    def _5xy0(self):
+        return 'SKIP.EQ\t\tV%s, \tV%s' % self._extract_vx_vy()
 
     def _6XNN(self):
-        return 'MVI \t\tV%s, \t$%s' % self.extract_vx_nn()
+        return 'MVI \t\tV%s, \t$%s' % self._extract_vx_nn()
 
     def _7XNN(self):
-        return 'ADI \t\tV%s, \t$%s' % self.extract_vx_nn()
+        return 'ADI \t\tV%s, \t$%s' % self._extract_vx_nn()
 
     def _8ZZZ(self):
-        return self.op_map[(self.opcode & 0xf00f) + 0xff0]()
+        function = self.op_map[(self.opcode & 0xf00f) + 0xff0]
+        return function()
 
     def _8XY0(self):
-        return 'MOV \t\tV%s, \tV%s' % self.extract_vx_vy()
+        return 'MOV \t\tV%s, \tV%s' % self._extract_vx_vy()
 
     def _8XY1(self):
-        return 'OR  \t\tV%s, \tV%s' % self.extract_vx_vy()
+        return 'OR  \t\tV%s, \tV%s' % self._extract_vx_vy()
 
     def _8XY2(self):
-        return 'AND \t\tV%s, \tV%s' % self.extract_vx_vy()
+        return 'AND \t\tV%s, \tV%s' % self._extract_vx_vy()
 
     def _8XY3(self):
-        return 'XOR \t\tV%s, \tV%s' % self.extract_vx_vy()
+        return 'XOR \t\tV%s, \tV%s' % self._extract_vx_vy()
 
     def _8XY4(self):
-        return 'ADD.\t\tV%s, \tV%s' % self.extract_vx_vy()
+        return 'ADD.\t\tV%s, \tV%s' % self._extract_vx_vy()
 
     def _8XY5(self):
-        return 'SUB.\t\tV%s, \tV%s' % self.extract_vx_vy()
+        return 'SUB.\t\tV%s, \tV%s' % self._extract_vx_vy()
 
     def _8XY6(self):
-        return 'SHR.\t\tV%s' % self.extract(0xf00)[2]
+        return 'SHR.\t\tV%s' % self._extract(0xf00)[2]
 
     def _8XY7(self):
-        return 'SBB.\t\tV%s, \tV%s' % self.extract_vx_vy()
+        return 'SBB.\t\tV%s, \tV%s' % self._extract_vx_vy()
 
     def _8XYE(self):
-        return 'SHL.\t\tV%s' % self.extract(0xf00)[2]
+        return 'SHL.\t\tV%s' % self._extract(0xf00)[2]
 
     def _9XY0(self):
-        return 'SKIP.NE\t\tV%s, \tV%s' % self.extract_vx_vy()
+        return 'SKIP.NE\t\tV%s, \tV%s' % self._extract_vx_vy()
 
     def _ANNN(self):
-        return 'MVI \t\tI,\t\t$%s' % self.extract(0xfff)[2:]
+        return 'MVI \t\tI,\t\t$%s' % self._extract(0xfff)[2:]
 
     def _BNNN(self):
-        return 'JUMP\t\t$%s(V0)' % self.extract(0xfff)[2:]
+        return 'JUMP\t\t$%s(V0)' % self._extract(0xfff)[2:]
 
     def _CXNN(self):
-        return 'RAND\t\tV%s, \t$%s' % self.extract_vx_nn()
+        return 'RAND\t\tV%s, \t$%s' % self._extract_vx_nn()
 
     def _DNNN(self):
-        vx, vy = self.extract_vx_vy()
-        height = self.extract(0xf)[2]
+        vx, vy = self._extract_vx_vy()
+        height = self._extract(0xf)[2]
         return 'DRAW\t\tVX:%s\tVY:%s\tH:%s' % (vx, vy, height)
 
     def _EZZZ(self):
-        return self.op_map[self.opcode & 0xf00f]()
+        function = self.op_map[self.opcode & 0xf00f]
+        return function()
 
     def _EX9E(self):
-        return 'SKIP.KEY\tV%s' % self.extract(0xf00)[2]
+        return 'SKIP.KEY\tV%s' % self._extract(0xf00)[2]
 
     def _EXA1(self):
-        return 'SKIP.NOKEY\tV%s' % self.extract(0xf00)[2]
+        return 'SKIP.NOKEY\tV%s' % self._extract(0xf00)[2]
 
     def _FZZZ(self):
-        print self.opcode
-        return self.op_map[self.opcode & 0xf0ff]()
+        function = self.op_map[self.opcode & 0xf0ff]
+        return function()
 
     def _FX07(self):
-        return 'MOV \t\tV%s, \tDELAY' % self.extract(0xf00)[2]
+        return 'MOV \t\tV%s, \tDELAY' % self._extract(0xf00)[2]
 
     def _FX0A(self):
-        return 'WAITKEY\t\tV%s' % self.extract(0xf00)[2]
+        return 'WAITKEY\t\tV%s' % self._extract(0xf00)[2]
 
     def _FX15(self):
-        return 'MOV \t\tDELAY, \tV%s' % self.extract(0xf00)[2]
+        return 'MOV \t\tDELAY, \tV%s' % self._extract(0xf00)[2]
 
     def _FX18(self):
-        return 'MOV \t\tSOUND, \tV%s' % self.extract(0xf00)[2]
+        return 'MOV \t\tSOUND, \tV%s' % self._extract(0xf00)[2]
 
     def _FX1E(self):
-        return 'ADD \t\tI,\t\tV%s' % self.extract(0xf00)[2]
+        return 'ADD \t\tI,\t\tV%s' % self._extract(0xf00)[2]
 
     def _FX29(self):
-        return 'SPRITE\t\tV%s' % self.extract(0xf00)[2]
+        return 'SPRITE\t\tV%s' % self._extract(0xf00)[2]
 
     def _FX33(self):
-        return 'MOVBCD\t\tV%s' % self.extract(0xf00)[2]
+        return 'MOVBCD\t\tV%s' % self._extract(0xf00)[2]
 
     def _FX55(self):
-        return 'MOVM\t\t(I), \tV0-V%s' % self.extract(0xf00)[2]
+        return 'MOVM\t\t(I), \tV0-V%s' % self._extract(0xf00)[2]
 
     def _FX65(self):
-        return 'MOVM\t\tV0-V%s,\t(I)' % self.extract(0xf00)[2]
-
-if __name__ == '__main__':
-    dsm = Disassembler('utils/games/Pong.ch8')
-    dsm.disassemble_all()
+        return 'MOVM\t\tV0-V%s,\t(I)' % self._extract(0xf00)[2]
