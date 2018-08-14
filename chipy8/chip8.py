8606869624f8dd48c167afe85feaf730c5c85d0e
Clayton Powell <daustoe@gmail.com>
2018-08-13 22:51:42 -0700

    moving to Tkinter
    have tkinter window being generated, key handler is working.

--- chipy8/chip8.py
+++ chipy8/chip8.py
@@ -1,39 +1,45 @@
 import chipy8.cpu as cpu
-import pyglet
+from tkinter import *
 
 
-class Chip8(pyglet.window.Window):
+class Chip8(Canvas):
     """
     Chip 8 emulator class. Subclasses pyglet Window class and contains the cpu of the Chip 8 interpreter.
     :param args:
     :param kwargs:
     """
 
-    def __init__(self, *args, **kwargs):
-        super(Chip8, self).__init__(*args, **kwargs)
-        self.key_map = {pyglet.window.key._1: 0x1,
-                        pyglet.window.key._2: 0x2,
-                        pyglet.window.key._3: 0x3,
-                        pyglet.window.key._4: 0xc,
-                        pyglet.window.key.Q: 0x4,
-                        pyglet.window.key.W: 0x5,
-                        pyglet.window.key.E: 0x6,
-                        pyglet.window.key.R: 0xd,
-                        pyglet.window.key.A: 0x7,
-                        pyglet.window.key.S: 0x8,
-                        pyglet.window.key.D: 0x9,
-                        pyglet.window.key.F: 0xe,
-                        pyglet.window.key.Z: 0xa,
-                        pyglet.window.key.X: 0,
-                        pyglet.window.key.C: 0xb,
-                        pyglet.window.key.V: 0xf}
+    def __init__(self, master=None, width=200, height=200):
+        Canvas.__init__(self, master=None)
+        self.master = master
+        self.master.title('Chip 8')
+        self.cpu = cpu.CPU(self)
+        print('test')
+
+        self.key_map = {'1': 0x1,
+                        '2': 0x2,
+                        '3': 0x3,
+                        '4': 0xc,
+                        'q': 0x4,
+                        'w': 0x5,
+                        'e': 0x6,
+                        'r': 0xd,
+                        'a': 0x7,
+                        's': 0x8,
+                        'd': 0x9,
+                        'f': 0xe,
+                        'z': 0xa,
+                        'x': 0,
+                        'c': 0xb,
+                        'v': 0xf}
+        '''
         self.white_pixel = pyglet.image.load('chipy8/resources/pixel.png')
         pattern = pyglet.image.SolidColorImagePattern(color=(0, 0, 0, 0))
         self.black_pixel = pattern.create_image(10, 10)
         self.beep = pyglet.resource.media('chipy8/resources/buzz.wav', streaming=False)
-        self.cpu = cpu.CPU(self)
         self.clear()
         self.set_vsync(False)
+        '''
 
     def load_rom(self, rom_path):
         """
@@ -52,24 +58,22 @@ class Chip8(pyglet.window.Window):
             self.dispatch_events()
             self.cpu.cycle()
 
-    def on_key_press(self, symbol, modifiers):
+    def on_key_press(self, event):
         """
-        Determines what should be done on a key press. Overrides the pyglet Window definition of this method. Looks up
-        key in self.key_map and hands it to the cpu to use with opcodes.
-        :param symbol:
-        :param modifiers:
+        Determines what should be done on a key press. Grabs events, and if a
+        valid key is pressed for the Chip-8 interpreter, we pass it alongself.
+        :param event:
         """
-        if symbol in self.key_map.keys():
-            self.cpu.key_inputs[self.key_map[symbol]] = 1
+        if event.char in self.key_map.keys():
+            self.cpu.key_inputs[self.key_map[event.char]] = 1
 
-    def on_key_release(self, symbol, modifiers):
+    def on_key_release(self, event):
         """
         Same as on_key_press but with key release.
-        :param symbol:
-        :param modifiers:
+        :param event:
         """
-        if symbol in self.key_map.keys():
-            self.cpu.key_inputs[self.key_map[symbol]] = 0
+        if event.char in self.key_map.keys():
+            self.cpu.key_inputs[self.key_map[event.char]] = 0
 
     def draw_pixel(self, x, y):
         """
--- main.py
+++ main.py
@@ -1,36 +1,14 @@
 from chipy8 import chip8
-import pyglet
+from tkinter import *
 import argparse
 
-
-def intro_update(dt):
-    """
-    Update method for both intro sequence.
-    :param dt:
-    """
-    if emulator.cpu.opcode != 0x1210:
-        emulator.cpu.cycle()
-    else:
-        pyglet.clock.unschedule(intro_update)
-        pyglet.clock.schedule_once(start, 3)
+root = Tk()
+root.geometry("400x300")
 
 
-def start(dt):
-    """
-    loads the chosen rom for the chip8 emulator
-    :param dt:
-    """
-    emulator.clear()
-    emulator.load_rom(args.rom)
-    pyglet.clock.schedule_interval(emulator.main, 1/1000)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("rom", type=str, help="File path to the Chip8 Rom you wish to run.")
-    args = parser.parse_args()
-    template = pyglet.gl.Config(double_buffer=False)
-    emulator = chip8.Chip8(640, 320, config=template, caption="Chip 8 Emulator")
-    emulator.load_rom('chipy8/resources/programs/Chip8 emulator Logo.ch8')
-    pyglet.clock.schedule(intro_update)
-    pyglet.app.run()
+app = chip8.Chip8(root)
+app.bind("<Key>", app.on_key_press)
+app.bind("<KeyRelease>", app.on_key_release)
+app.focus_set()
+app.pack()
+root.mainloop()
